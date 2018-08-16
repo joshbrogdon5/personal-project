f90c236e9dd56613771a7479fc3955e016490399
Josh <joshbrogdon5@gmail.com>
2018-08-16 16:01:22 -0600

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,7 +12,7 @@ class Login extends Component {
 
         let url = `${window.location.origin}/auth/callback`
 
-        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri$={url}&response_type=code`
+        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
     }
 
     render(){
